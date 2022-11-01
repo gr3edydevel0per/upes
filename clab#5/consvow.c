829c1f907f0ed53d707822c6b82d49956fc4011d
Kanav Arora <69228499+realkanavarora@users.noreply.github.com>
2022-11-01 11:38:27 +0530

    Create consvow.c

---
+++ clab#5/consvow.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main()  {
+ 
+ char ch;
+ printf("Please enter the charactrer to check \n");
+ scanf("%c",&ch);
+ switch(){
+ 
+  case 'a' : {
+   printf("Entered character is vowel")
+  }
+ 
+   case 'e' : {
+   printf("Entered character is vowel")
+  }  case 'i': {
+   printf("Entered character is vowel")
+  }  case 'o': {
+   printf("Entered character is vowel")
+  }
+    case 'u': {
+   printf("Entered character is vowel")
+  }
+    case 'A': {
+   printf("Entered character is vowel")
+  }
+    case 'E': {
+   printf("Entered character is vowel")
+  }
+    case 'I': {
+   printf("Entered character is vowel")
+  }
+    case 'O': {
+   printf("Entered character is vowel")
+  }
+    case 'U' :{
+   printf("Entered character is vowel")
+  }
+  default :{
+   printf("Given character is consonant")
+  }
+ 
+ 
+ 
+ 
+ 
+ }
+
+
+
+
+}
