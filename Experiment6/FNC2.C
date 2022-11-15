fea500fa0a0e72aacd986ce80e1f509ded6d0d55
Kanav Arora <69228499+realkanavarora@users.noreply.github.com>
2022-11-15 12:06:07 +0530

    Create FNC2.C

---
+++ Experiment6/FNC2.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int triangle_area(int,int);
+void swap(int*,int*);
+float* remainder(a,b);
+
+int main(){
+    int at,a,b;
+    printf("Please enter the numbers to perform operations :\n ");
+    scanf("%d%d",&a,&b);
+    at= triangle_area(a,b);
+    printf("The area of triangle is %d\n",at);
+    swap(&a,&b);
+    printf("The numebrs after swapping are a is %d and b is %d\n",a,b);
+    
+    
+    
+}
+
+
+int triangle_area(int base,int height){
+    
+    int area;
+    area=(base*height)/2;
+    return area;
+}
+void swap(int *x , int *y){
+    
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+}
