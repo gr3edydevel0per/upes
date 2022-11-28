43656b166b147274f1572ad998206abffbb065bf
Kanav Arora <69228499+realkanavarora@users.noreply.github.com>
2022-11-28 15:09:13 +0530

    Update binarySearch.c

--- Experiment#8/binarySearch.c
+++ Experiment#8/binarySearch.c
@@ -1,26 +1,24 @@
-#inlcude <stdio.h>
+#include <stdio.h>
 
 int binarySearch(int arr[], int target, int start , int end){
-    int mid = (start+end)/2;
-    if (arr[mid]==target)
-        return mid
-    else if (target>mid){
-      start=mid+1;
-      return binarySearch(arr[],target,start,end    
+    int mid = start+(end-end)/2;
+    if (arr[mid]==target){
+        return mid;}
+    else if (target>arr[mid]){
+      return binarySearch(arr,target,mid+1,end);   
     }
-    else if (target<mid){
-      endd=mid-1;
-      return binarySearch(arr[],target,start,end    
+    else if (target<arr[mid]){
+      return binarySearch(arr,target,start,mid-1); 
     }
     else{
-     return -1
+     return -1;
     }
 }
 
 int main() {
-  int arr[10] = {12,34,54,65,78,95,100,123,329,500}
-  a = binarySearch(arr,54,0,9)
-
-
-
+  int arr[] = {12,34,54,65,78,95,100,123,329,500,969};
+  int a;
+  int n= sizeof(arr)/sizeof(arr[0]);
+  a = binarySearch(arr,95,0,n-1);
+  printf("%d",a);
 }
