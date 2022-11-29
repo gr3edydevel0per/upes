b8a59cee9097472190b5cd39d724fa4ba55145b5
Kanav Arora <69228499+realkanavarora@users.noreply.github.com>
2022-11-29 11:18:47 +0530

    Update sumArray.c

--- Experiment#8/sumArray.c
+++ Experiment#8/sumArray.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main() {
-  int arr[4][4];
+  int arr[4][4], sum_row[4], sum_col[4];
   for(int i=0;i<4;i++){
     for(int j=0;j<4;j++){
-       scanf("%d",arr[i][j])
+       scanf("%d",arr[i][j]);
     }
 }
+  for(int j=0;j<4;j++0){
+    for(int k=0;k<4;k++){
+        arr_row[j]+=arr[j][k]
+    }
+    printf("%d",arr_row[j])
+  }
+  
+  
 }
