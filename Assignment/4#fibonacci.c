8c5a6ceb4de602eb6236ed8df803509f1b22da86
Kanav Arora <69228499+realkanavarora@users.noreply.github.com>
2022-12-05 15:53:36 +0530

    Update 4#fibonacci.c

--- Assignment/4#fibonacci.c
+++ Assignment/4#fibonacci.c
@@ -7,7 +7,18 @@ int main() {
   int num;
   printf("Enter nth number to print series \n");
   scanf("%d",&num);
+  printf("Fibonacci series upto %d term\n",num);
+  for(int i=1;i<=num;i++)
+     printf("%d \n",fibonacci(i));
   
 
 
 }
+
+int fibonacci(int x){
+  if (x<=2)
+   return 1;
+  else
+    return fibonacci(x-1)+fibonacci(x-2);
+      
+  }
