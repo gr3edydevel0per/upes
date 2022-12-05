b240c14c8c73784725c0f5f58ee3f9562f8d8cc8
Kanav Arora <69228499+realkanavarora@users.noreply.github.com>
2022-12-05 18:56:15 +0530

    Create 6#BinarySearch.c

---
+++ Assignment/6#BinarySearch.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int l, int end, int x)
+{
+    if (end >= l) {
+        int mid = l + (end - l) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+ 
+        return binarySearch(arr, mid + 1, end, x);
+    }
+    return -1;
+}
+ 
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 ,90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
+    int result = binarySearch(arr, 0, n - 1, x);
+    if (result!=-1)
+        printf("Element is present at index %d", result);
+    else
+        printf("Element is not present in the list");
+    return 0;
+}
