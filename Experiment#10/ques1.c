4d299e920e948293aed441d25e263a32c1e45713
Kanav Arora <69228499+realkanavarora@users.noreply.github.com>
2022-12-12 16:03:05 +0530

    Update ques1.c

--- Experiment#10/ques1.c
+++ Experiment#10/ques1.c
@@ -1,26 +1,40 @@
-#include <stdio.h0>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct subject {
     char name[100];
     char code[10];
+    int credit_hours;
 };
 
 int main() {
-  int n;
- struct subject *s = malloc(sizeof(struct subject));
-  printf("Please enter number of subjects \n");
-  scanf("%d",&n);
+    int n;
+    printf("Enter the number of subjects: ");
+    scanf("%d", &n);
+
+    // Allocate dynamic memory for 'n' subjects
+    struct subject *s = (struct subject *)malloc(n * sizeof(struct subject));
+
+    // Obtain details of subjects using a for loop
     for (int i = 0; i < n; i++) {
-    printf("Enter subject name: ");
-    scanf("%s", s[i].name);
+        printf("Enter the details of subject %d: \n", i+1);
+        printf("Subject name: ");
+        scanf("%s", s[i].name);
+        printf("Subject code: ");
+        scanf("%s", s[i].code);
+        printf("Credit hours: ");
+        scanf("%d", &s[i].credit_hours);
+    }
 
-    printf("Enter subject code: ");
-    scanf("%s", s[i].code);
+    // Print the details of subjects
+    printf("Subject details: \n");
+    for (int i = 0; i < n; i++) {
+        printf("Subject %d: \n", i+1);
+        printf("Name: %s \n", s[i].name);
+        printf("Code: %s \n", s[i].code);
+        printf("Credit hours: %d \n", s[i].credit_hours);
+    }
 
-    // other fields for subject details
-}
-
-  
-
-
+    return 0;
 }
