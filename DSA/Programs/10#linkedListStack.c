523cae91ae2d43ecf1c82b3007b3a0cae7631431
realkanavarora <arora.kanav77@gmail.com>
2023-04-20 12:27:08 +0530

    queues and stack implementation

--- DSA/Programs/10#linkedListStack.c
+++ DSA/Programs/10#linkedListStack.c
@@ -1,63 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct ListNode {
-    int data;
-    struct ListNode *next;
-};
-
-void display(struct ListNode *head);
-void push(struct ListNode **head);
-void pop(struct ListNode **head);
-void seek(struct ListNode *head);
-
-int main() {
-    struct ListNode *head = NULL;
-    push(&head);
-    display(head);
-    pop(&head);
-    display(head);
-    seek(head);
-    return 0;
+// Structure to create a node with data and the next pointer
+struct node
+{
+    int info;
+    struct node *ptr;
+} *top, *top1, *temp;
+int count = 0;
+// Push() operation on a stack
+void push(int data)
+{
+    if (top == NULL)
+    {
+        top = (struct node *)malloc(1 * sizeof(struct node));
+        top->ptr = NULL;
+        top->info = data;
+    }
+    else
+    {
+        temp = (struct node *)malloc(1 * sizeof(struct node));
+        temp->ptr = top;
+        temp->info = data;
+        top = temp;
+    }
+    count++;
+    printf("Node is Inserted\n\n");
 }
-
-void push(struct ListNode **head) {
-    int num;
-    printf("Please enter the data to push into stack : ");
-    scanf("%d", &num);
-    struct ListNode *temp = (struct ListNode*) malloc(sizeof(struct ListNode));
-    temp->data = num;
-    temp->next = *head;
-    *head = temp;
+int pop()
+{
+    top1 = top;
+    if (top1 == NULL)
+    {
+        printf("\nStack Underflow\n");
+        return -1;
+    }
+    else
+        top1 = top1->ptr;
+    int popped = top->info;
+    free(top);
+    top = top1;
+    count--;
+    return popped;
 }
-
-void pop(struct ListNode **head) {
-    if (*head == NULL) {
-        printf("Stack is empty.\n");
+void display()
+{
+    // Display the elements of the stack
+    top1 = top;
+    if (top1 == NULL)
+    {
+        printf("\nStack Underflow\n");
         return;
     }
-    struct ListNode *temp = *head;
-    *head = (*head)->next;
-    free(temp);
-}
 
-void display(struct ListNode *head) {
-    if (head == NULL) {
-        printf("Stack is empty.\n");
-        return;
+    printf("The stack is \n");
+    while (top1 != NULL)
+    {
+        printf("%d--->", top1->info);
+        top1 = top1->ptr;
     }
-    printf("Stack elements: ");
-    while (head != NULL) {
-        printf("%d ", head->data);
-        head = head->next;
-    }
-    printf("\n");
+    printf("NULL\n\n");
 }
-
-void seek(struct ListNode *head) {
-    if (head == NULL) {
-        printf("Stack is empty.\n");
-        return;
+int main()
+{
+    int choice, value;
+    printf("\nImplementation of Stack using Linked List\n");
+    while (1)
+    {
+        printf("\n1. Push\n2. Pop\n3. Display\n4. Exit\n");
+        printf("\nEnter your choice : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter the value to insert: ");
+            scanf("%d", &value);
+            push(value);
+            break;
+        case 2:
+            printf("Popped element is :%d\n", pop());
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+            break;
+        default:
+            printf("\nWrong Choice\n");
+        }
     }
-    printf("Top element: %d\n", head->data);
 }
--- DSA/Programs/11#dynamicQueue.c
+++ DSA/Programs/11#dynamicQueue.c
@@ -1,50 +1,203 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct ListNode {
-    int data;
-    struct ListNode *next;
-} *front = NULL, *rear = NULL,*temp;
+struct link
 
-void queue(struct ListNode *temp) {
-    printf("Please enter the data: ");
-    scanf("%d", &(temp->data));
-    if (rear == NULL) {
-        front = temp;
-        rear = temp;
-        temp->next = NULL;
-    } else {
-        rear->next = temp;
-        temp->next = NULL;
-        rear = temp;
+{
+
+    int info;
+
+    struct link *next;
+
+} *front, *rear;
+
+void insert_q(int no);
+
+int delete_q();
+
+void display();
+
+int main()
+
+{
+
+    int ch, no;
+
+    rear = NULL;
+
+    front = NULL;
+
+    while (1)
+
+    {
+
+        printf("\n Dynamic Queue Menu");
+
+        printf("\n 1->Insert ");
+
+        printf("\n 2->Delete ");
+
+        printf("\n 3->Display");
+
+        printf("\n 4->Exit   ");
+
+        printf("\n enter your choice :  ");
+
+        scanf("%d", &ch);
+
+        switch (ch)
+
+        {
+
+        case 1:
+
+        {
+
+            printf("Enter The Element Value\n");
+
+            scanf("%d", &no);
+
+            insert_q(no);
+
+            printf("\n queue after insertion");
+
+            display();
+
+            break;
+        }
+
+        case 2:
+
+        {
+
+            no = delete_q();
+
+            printf("\n The deleted element = %d", no);
+
+            printf("\n queue after deletion");
+
+            display();
+
+            break;
+        }
+
+        case 3:
+
+        {
+
+            printf("\n Queue is as follow");
+
+            display();
+
+            break;
+        }
+
+        case 4:
+
+        {
+
+            printf("Exit....!!!!");
+
+
+            exit(0);
+
+            break;
+        }
+
+        default:
+
+            printf("\n Wrong choice...!!!! ");
+        }
     }
 }
 
-void dequeue() {
-    if (front == NULL) {
-        printf("Queue is empty.\n");
-    } else if (front == rear) {
-        front = NULL;
-        rear = NULL;
-    } else {
+void insert_q(int no)
+
+{
+
+    struct link *new1;
+
+    new1 = (struct link *)malloc(sizeof(struct link));
+
+    new1->info = no;
+
+    new1->next = NULL;
+
+    if (rear == NULL || front == NULL)
+
+    {
+
+        front = new1;
+    }
+
+    else
+
+    {
+
+        rear->next = new1;
+    }
+
+    rear = new1;
+}
+
+int delete_q()
+
+{
+
+    struct link *t;
+
+    int no;
+
+    if (front == NULL || rear == NULL)
+
+    {
+
+        printf("\n queue is Under Flow");
+
+        
+
+        return -1;
+    }
+
+    else
+
+    {
+
+        t = front;
+
+        no = t->info;
+
         front = front->next;
+
+        free(t);
     }
+
+    return (no);
 }
 
-void display(){
-    temp=front;
-    while(temp!=NULL){
-        printf("%d ",temp->data);
-        temp=temp->next;
-    }
-}
+void display()
 
-int main() {
-    struct ListNode *temp = (struct ListNode*) malloc(sizeof(struct ListNode));
-    queue(temp);
-    display();
-    dequeue();
-    printf("Deleted");
-    display();
-    return 0;
+{
+
+    struct link *t;
+
+    t = front;
+
+    if (front == NULL || rear == NULL)
+
+    {
+
+        printf("\nQueue is empty");
+
+        exit(0);
+    }
+
+    while (t != NULL)
+
+    {
+
+        printf("\n %d", t->info);
+
+        t = t->next;
+    }
 }
